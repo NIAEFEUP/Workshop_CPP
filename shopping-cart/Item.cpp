07b5d59334f7ed0b06692f62d08f54928cbd72c1
André Moreira <andre.moreira.9@hotmail.com>
2022-03-07 15:53:16 +0000

    Refactor exercise to classes

---
+++ shopping-cart/Item.cpp
@@ -0,0 +1,5 @@
+//
+// Created by andremoreira9 on 3/7/22.
+//
+
+#include "Item.h"
---
+++ shopping-cart/Item.h
@@ -0,0 +1,13 @@
+//
+// Created by andremoreira9 on 3/7/22.
+//
+
+#ifndef WORKSHOP_CPP_ITEM_H
+#define WORKSHOP_CPP_ITEM_H
+
+using namespace std;
+
+
+
+
+#endif //WORKSHOP_CPP_ITEM_H
