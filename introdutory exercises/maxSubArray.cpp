2c3e3009e8bad81ef59373aaac725167d41f428e
sirkotal <lordkotal2002@gmail.com>
2023-02-14 16:45:14 +0000

    Added another vectors exercise

--- introdutory exercises/maxSubArray.cpp
+++ introdutory exercises/maxSubArray.cpp
@@ -15,23 +15,23 @@ int main() {
     vector<int> v3 = {2, 4, 6, 8, 10};
 
     if (maxSubArray(v1) == 7) {
-        cout << "Your program has passed the v1 test! " << "=> " << maxSubArray(v1) << " (answer) = " << 7  << " (result)" <<  endl;
+        cout << "Your program has passed the v1 test! " << "=> " << maxSubArray(v1) << " (answer) = " << 7 << " (result)" <<  endl;
     }
     else {
-        cout << "Your program has failed the v1 test! " << "=> " << maxSubArray(v1) << " (answer) ≠ " << 7  << " (result)" <<  endl;
+        cout << "Your program has failed the v1 test! " << "=> " << maxSubArray(v1) << " (answer) ≠ " << 7 << " (result)" <<  endl;
     }
 
     if (maxSubArray(v2) == -1) {
-        cout << "Your program has passed the v2 test! " << "=> " << maxSubArray(v2) << " (answer) = " << -1  << " (result)" <<  endl;
+        cout << "Your program has passed the v2 test! " << "=> " << maxSubArray(v2) << " (answer) = " << -1 << " (result)" <<  endl;
     }
     else {
-        cout << "Your program has failed the v2 test! " << "=> " << maxSubArray(v2) << " (answer) ≠ " << -1  << " (result)" <<  endl;
+        cout << "Your program has failed the v2 test! " << "=> " << maxSubArray(v2) << " (answer) ≠ " << -1 << " (result)" <<  endl;
     }
 
     if (maxSubArray(v3) == 30) {
-        cout << "Your program has passed the v3 test! " << "=> " << maxSubArray(v3) << " (answer) = " << 30  << " (result)" <<  endl;
+        cout << "Your program has passed the v3 test! " << "=> " << maxSubArray(v3) << " (answer) = " << 30 << " (result)" <<  endl;
     }
     else {
-        cout << "Your program has failed the v3 test! " << "=> " << maxSubArray(v3) << " (answer) ≠ " << 30  << " (result)" <<  endl;
+        cout << "Your program has failed the v3 test! " << "=> " << maxSubArray(v3) << " (answer) ≠ " << 30 << " (result)" <<  endl;
     }
 }
